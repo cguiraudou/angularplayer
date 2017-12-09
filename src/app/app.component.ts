c05da6bc69ab0c6a82f087bd869a9ae21df421b6
unknown <colin.guiraudou@insa-rennes.fr>
2017-12-10 00:09:09 +0100

    ajout input + player, binding url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  url:string;
+  sUrl: SafeUrl;
+  
+  constructor(private sanitizer: DomSanitizer){
+    this.url = `https://www.youtube.com/embed/IVEwemk-TRU`;
+    this.sUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
+  }
+  
+  refresh(){
+    this.sUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
+  }
+  
 }
